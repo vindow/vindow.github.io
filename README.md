080b5baf9a18d303f7ce99a8ec9af56ad63ebbf9
Alvin Yang <alvin.yang73@gmail.com>
2019-03-21 19:11:43 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Website
-This is a the source code for my personal website. Why are you looking here instead of at the [**website**](https://vindow.github.io/)?
---
+++ precache-manifest.acbef934713462ba027f0a45051342f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37dde631d4eb87f06a53",
+    "url": "/static/js/main.f5d45189.chunk.js"
+  },
+  {
+    "revision": "05bcf9a2c4409e728398",
+    "url": "/static/js/2.1e66a06b.chunk.js"
+  },
+  {
+    "revision": "37dde631d4eb87f06a53",
+    "url": "/static/css/main.6a8ace35.chunk.css"
+  },
+  {
+    "revision": "05bcf9a2c4409e728398",
+    "url": "/static/css/2.cf05f4b9.chunk.css"
+  },
+  {
+    "revision": "36609a348298dd4d0b5983bd3e5c5550",
+    "url": "/index.html"
+  }
+];
