eb2a96eacce9965fa9ba7410b2680d4104b7bb38
Alvin Yang <alvin.yang73@gmail.com>
2020-05-05 16:07:54 -0700

    Updates

---
+++ precache-manifest.3c61eda181893cccddaa820b70507c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0d8f10f361e3a1e9ddf89c944920b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a19f73b639d907be2ab",
+    "url": "/static/css/2.c6512ac3.chunk.css"
+  },
+  {
+    "revision": "391295a424e40ee7c368",
+    "url": "/static/css/main.87f85fd2.chunk.css"
+  },
+  {
+    "revision": "2a19f73b639d907be2ab",
+    "url": "/static/js/2.a1a17da4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a1a17da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391295a424e40ee7c368",
+    "url": "/static/js/main.fdf93cf4.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
